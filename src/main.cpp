bde7aec008dc3e9b2365e4bf6a345e738a81f6e2
Andrea Franceschini <andrea.franceschini@gmail.com>
2020-07-28 17:54:40 +0200

    Add license

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+/** MIDI Faders
+    Copyright (C) 2020 Andrea Franceschini <andrea.franceschini@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <LiquidCrystal_I2C.h>
 #include <IoAbstraction.h>
 #include <Rotary.h>
